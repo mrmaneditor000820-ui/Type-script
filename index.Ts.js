648c1bb5fc71f6b6d548b9df130202d76145e042
mrmaneditor000820-ui <mrmaneditor000820@gmail.com>
2026-04-10 22:47:31 +0500

    type script assigmnet completed

--- index.Ts.js
+++ index.Ts.js
@@ -1,4 +1,40 @@
 "use strict";
-// tuple (order is compulsory)
-var arr = [1, "ali", true];
-console.log(arr);
+//     Interface 
+let user = {
+    name: "Rehman",
+    age: 20
+};
+console.log(user.age);
+//    Pramative Types
+// string
+var myname = "Abdurehman";
+console.log(myname);
+// Number 
+var myage = 18;
+console.log(myage);
+// Boleans 
+let isPaid = true;
+if (isPaid) {
+    console.log("Access granted");
+}
+else {
+    console.log("Access denied");
+}
+// Arrays
+let names = ["Ali", "Ahmed"];
+console.log(names);
+// objects
+let obj = {
+    name: "Rehman",
+    age: 20
+};
+console.log(obj.name);
+// functions
+function add(a, b) {
+    return a + b;
+}
+console.log(add(2, 5));
+function greet() {
+    console.log("Hello Rehman");
+}
+greet();
